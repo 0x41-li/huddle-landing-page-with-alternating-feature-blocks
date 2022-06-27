b93368a0d610ba613066d63fa078d88b1d7dcecf
Ali Sbane <ali.sbane.dev@gmail.com>
2022-06-27 05:56:37 +0100

    First Section completed for mobile devices

--- src/section/first-section/FirstSection.js
+++ src/section/first-section/FirstSection.js
@@ -1,8 +1,59 @@
+import {
+  Box,
+  BoxH2,
+  BoxPara,
+  BoxTextWrapper,
+  FlowingConversationsBoxImage,
+  GrowTogetherBoxImage,
+  Section,
+  YourUsersBoxImage,
+} from "./FirstSection.style";
+
+// images
+import growTogetherImage from "../../assets/images/grow-together-illustr.png";
+import flowingConversationsImage from "../../assets/images/flowing-conversations-illustr.png";
+import yourUsersImage from "../../assets/images/your-users-illustr.png";
 
 const FirstSection = () => {
   return (
-    <div>FirstSection</div>
-  )
-}
+    <Section className="container">
+      <Box>
+        <GrowTogetherBoxImage src={growTogetherImage} />
+        <BoxTextWrapper>
+          <BoxH2 className="poppins-semibold">Grow Together</BoxH2>
+          <BoxPara className="open-sans-regular">
+            Generate meaningful discussions with your audience and build a
+            strong, loyal community. Think of the insightful conversations you
+            miss out on with a feedback form.
+          </BoxPara>
+        </BoxTextWrapper>
+      </Box>
 
-export default FirstSection
+      <Box>
+        <FlowingConversationsBoxImage src={flowingConversationsImage} />
+        <BoxTextWrapper>
+          <BoxH2 className="poppins-semibold">Flowing Conversations</BoxH2>
+          <BoxPara className="open-sans-regular">
+            You wouldn't paginate a conversation in real life, so why do it
+            online? Our threads have just-in-time loading for a more natural
+            flow.
+          </BoxPara>
+        </BoxTextWrapper>
+      </Box>
+
+      <Box>
+        <YourUsersBoxImage src={yourUsersImage} />
+        <BoxTextWrapper>
+          <BoxH2 className="poppins-semibold">Your Users</BoxH2>
+          <BoxPara className="open-sans-regular">
+            It takes no time at all to integrate Huddle with your app's
+            authentication solution. This means, once signed in to your app,
+            your users can start chatting immediately.
+          </BoxPara>
+        </BoxTextWrapper>
+      </Box>
+    </Section>
+  );
+};
+
+export default FirstSection;
--- src/section/first-section/FirstSection.style.js
+++ src/section/first-section/FirstSection.style.js
@@ -1,1 +1,114 @@
+import styled from "styled-components";
 
+export const Section = styled.section`
+  padding-top: 59px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`;
+
+export const Box = styled.div`
+  padding: 56px 39px 0px 40px;
+
+  width: 335px;
+  height: 488px;
+
+  margin: 0 auto;
+
+  background: #ffffff;
+  box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.07);
+  border-radius: 16px;
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 100%;
+    height: auto;
+    padding: 56px 39px 56px 40px;
+  }
+`;
+
+export const BoxTextWrapper = styled.div`
+  margin-top: 54px;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 0;
+    flex: 0 1 50%;
+  }
+`;
+
+export const BoxH2 = styled.h2`
+  font-size: 20px;
+  line-height: 30px;
+
+  text-align: center;
+
+  color: #00252e;
+
+  @media screen and (min-width: 768px) {
+    text-align: left;
+  }
+`;
+
+export const BoxPara = styled.p`
+  font-size: 14px;
+  line-height: 21px;
+
+  text-align: center;
+
+  color: #808e9a;
+
+  margin-top: 16px;
+
+  @media screen and (min-width: 768px) {
+    text-align: left;
+  }
+`;
+
+export const GrowTogetherBoxImage = styled.img`
+  display: block;
+
+  width: 236px;
+  height: 194px;
+
+  margin: 0 auto;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+  }
+`;
+
+export const FlowingConversationsBoxImage = styled.img`
+  display: block;
+
+  width: 242.74px;
+  height: 172.8px;
+
+
+  margin: 0 auto;
+
+  margin-bottom: 66px;
+
+
+  @media screen and (min-width: 768px) {
+    order: 2;
+    margin: 0;
+  }
+`;
+
+export const YourUsersBoxImage = styled.img`
+  display: block;
+
+  width: 244.11px;
+  height: 184.69px;
+
+  margin: 0 auto;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+  }
+`;
