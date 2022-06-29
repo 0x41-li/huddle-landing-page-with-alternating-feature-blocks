55d4832fda3df71040f6f30a203c718359a65c3e
Ali Sbane <ali.sbane.dev@gmail.com>
2022-06-29 11:22:14 +0100

    First Section completed on all possible devices

--- src/section/first-section/FirstSection.style.js
+++ src/section/first-section/FirstSection.style.js
@@ -7,6 +7,10 @@ export const Section = styled.section`
   flex-direction: column;
   align-items: center;
   gap: 40px;
+
+  @media screen and (min-width: 1200px) {
+    padding-top: 160px;
+  }
 `;
 
 export const Box = styled.div`
@@ -38,6 +42,32 @@ export const Box = styled.div`
     height: auto;
     padding: 56px 39px 56px 40px;
   }
+
+  @media screen and (min-width: 992px) {
+    justify-content: space-around;
+  }
+
+  @media screen and (min-width: 1200px) {
+    height: 440px;
+    padding: 53px 32px 53px 120px;
+    justify-content: space-between;
+
+    &:nth-child(2) {
+      padding-right: 29px;
+    }
+
+    &:nth-child(3) {
+      padding-right: 26px;
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 1232px;
+
+    &:nth-child(2) > div {
+      flex: 0 0 499px;
+    }
+  }
 `;
 
 export const BoxTextWrapper = styled.div`
@@ -46,6 +76,10 @@ export const BoxTextWrapper = styled.div`
   @media screen and (min-width: 768px) {
     margin-top: 0;
     flex: 0 1 50%;
+  }
+
+  @media screen and (min-width: 1440px) {
+    flex: 0 0 488px;
   }
 `;
 
@@ -59,6 +93,11 @@ export const BoxH2 = styled.h2`
 
   @media screen and (min-width: 768px) {
     text-align: left;
+  }
+
+  @media screen and (min-width: 992px) {
+    font-size: 28px;
+    line-height: 42px;
   }
 `;
 
@@ -75,6 +114,11 @@ export const BoxPara = styled.p`
   @media screen and (min-width: 768px) {
     text-align: left;
   }
+
+  @media screen and (min-width: 992px) {
+    font-size: 18px;
+    line-height: 27px;
+  }
 `;
 
 export const GrowTogetherBoxImage = styled.img`
@@ -87,6 +131,13 @@ export const GrowTogetherBoxImage = styled.img`
 
   @media screen and (min-width: 768px) {
     margin: 0;
+    order: 2;
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 100%;
+    height: unset;
+    max-width: 407px;
   }
 `;
 
@@ -96,15 +147,18 @@ export const FlowingConversationsBoxImage = styled.img`
   width: 242.74px;
   height: 172.8px;
 
-
   margin: 0 auto;
 
   margin-bottom: 66px;
 
-
   @media screen and (min-width: 768px) {
-    order: 2;
     margin: 0;
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 100%;
+    max-width: 417.94px;
+    height: unset;
   }
 `;
 
@@ -118,5 +172,12 @@ export const YourUsersBoxImage = styled.img`
 
   @media screen and (min-width: 768px) {
     margin: 0;
+    order: 2;
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 100%;
+    max-width: 420.2px;
+    height: unset;
   }
 `;
